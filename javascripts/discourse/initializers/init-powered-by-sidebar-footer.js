dff65d186fb159c89a30ff7d4fa6d0cafa06ecdf
Chris <cdowney@gmail.com>
2025-02-13 14:14:00 -0500

    Remove powered-by sidebar component

--- javascripts/discourse/initializers/init-powered-by-sidebar-footer.js
+++
@@ -1,6 +0,0 @@
-import PoweredByDiscourse from "discourse/components/powered-by-discourse";
-import { apiInitializer } from "discourse/lib/api";
-
-// export default apiInitializer("1.8.0", (api) => {
-//   api.renderInOutlet("sidebar-footer-actions", PoweredByDiscourse);
-// });
